8b5eadb9003b89968cddf2d0f05c0bea472ca788
Connor William Roy Egan <connorwr.egan@gmail.com>
2020-06-26 18:48:05 +1000

    DoorKey Prototype 1
    GameObject will destroy itself if you click on it. Now when player inventory is done the key will be added to the inventory.

---
+++ MiniJam-Underground/Assets/Scripts/DoorKey.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorKey : MonoBehaviour
+{
+[SerializeField]
+GameObject doorToOpen;
+[SerializeField]
+GameObject player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseOver()
+    {   
+    if (Input.GetButtonDown("Fire1"))
+    {
+    Destroy(gameObject);
+    }
+    }
+}
