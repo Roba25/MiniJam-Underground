18a1277a5766de1afda1102d8c1354698169b571
Connor William Roy Egan <connorwr.egan@gmail.com>
2020-06-26 18:43:24 +1000

    PlayerController2D should work

---
+++ MiniJam-Underground/Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+//  Variables
+Rigidbody2D rb2d;
+
+[SerializeField]
+float moveSpeed = 5f;
+
+
+    void Start()
+    {
+    rb2d = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+    if (Input.GetKey("d"))
+    {
+    rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
+    } else if (Input.GetKey("a"))
+    {
+    rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
+    } else {
+    rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+    }
+    if (Input.GetKey("w"))
+    {
+    rb2d.velocity = new Vector2(rb2d.velocity.x, moveSpeed);
+    } else if (Input.GetKey("s"))
+    {
+    rb2d.velocity = new Vector2(rb2d.velocity.x, -moveSpeed);
+    }
+    }
+}
