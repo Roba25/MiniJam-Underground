125b5e46890c2411a7ad27d90abae6d39fef1913
Pouya Pournasir <33330581+PouyaJigsaw@users.noreply.github.com>
2020-06-26 15:55:08 +0430

    Added Audio Manager

---
+++ MiniJam-Underground/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AudioManager
+{
+  private static bool initialized = false;
+  private static AudioSource _audioSouce;
+  static Dictionary<AudioClipName ,AudioClip> _audioClips = new Dictionary<AudioClipName, AudioClip>();
+
+  public static bool Initialized => initialized;
+
+
+  public static void Initialize(AudioSource source)
+  {
+    initialized = true;
+    _audioSouce = source;
+    //_audioClips.Add(AudioClipName.GetKey, Resources.Load<AudioClip>("GetKey");
+    // and other audio clips added this way
+    
+    
+  }
+
+
+  public static void Play(AudioClipName name)
+  {
+    _audioSouce.PlayOneShot(_audioClips[name]);
+  }
+}
---
+++ MiniJam-Underground/Assets/Scripts/Audio/GameAudioSource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAudioSource : MonoBehaviour
+{
+   private void Awake()
+   {
+      if (!AudioManager.Initialized)
+      {
+         AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+         AudioManager.Initialize(audioSource);
+         DontDestroyOnLoad(gameObject);
+         
+      }
+
+      else
+      {
+         Destroy(gameObject);
+      }
+   }
+}
